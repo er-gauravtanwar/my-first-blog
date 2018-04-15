e135551913d704f97702d69019fbebd8713a0c18
er-gauravtanwar <er.gauravtanwar@gmail.com>
2018-04-15 13:12:43 +0530

    Add template dynamic post in template

--- blog/views.py
+++ blog/views.py
@@ -2,9 +2,11 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from models import Post
 
 # Create your views here.
 
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {"posts": posts})
